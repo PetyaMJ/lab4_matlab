c3db2f825ceef7c2a709dbdcd0557a8d6fa55032
PetyaMJ <32020921+PetyaMJ@users.noreply.github.com>
2017-11-30 19:56:45 +0300

    Delete zavdannya6_2.m

--- zavdannya6_2.m
+++
@@ -1,7 +0,0 @@
-x=[0:0.1:1.6];
-f=(exp(-x).*2)./(2.*x+x.^3)
-plot(f)
-title('name of graphic') %названня графіку
-xlabel('some x') %названня осі х
-ylabel('some y') %названня осі х
-legend('some function') %легенда
